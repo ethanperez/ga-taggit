e68b2e32802ddd9da55f0ce91509439e85657a98
Ethan Perez <ethan@ethanperez.com>
2015-08-17 21:50:42 -0500

    Upgrade and change window name

--- static/js/redirect.js
+++ static/js/redirect.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   var popupWindow = window.open(
       chrome.extension.getURL("static/index.html"),
-      "exampleName",
+      "GA Taggit",
       "width=500,height=600"
   );
   window.close();
